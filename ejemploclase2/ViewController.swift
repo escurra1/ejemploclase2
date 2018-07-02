4a01ea14dabef03153d2bc972680021b18c46d2e
mescurra <harol.josue.gomez.ruiz@everis.com>
2018-07-02 16:02:54 -0500

    eventos

--- ejemploclase2/ViewController.swift
+++ ejemploclase2/ViewController.swift
@@ -21,6 +21,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
 
 
 }
