b0823691c496c91eaa3502027087aff88990bba8
mescurra <harol.josue.gomez.ruiz@everis.com>
2018-07-02 16:28:13 -0500

    add viewcontroller

---
+++ ejemploclase2/PapsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PapsViewController.swift
+//  ejemploclase2
+//
+//  Created by mescurra on 7/2/18.
+//  Copyright © 2018 mescurra. All rights reserved.
+//
+
+import UIKit
+
+class PapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
