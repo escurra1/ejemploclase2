c4e145dfc44fcdfb7b714326f4b4d46ee6116321
mescurra <harol.josue.gomez.ruiz@everis.com>
2018-07-02 14:46:22 -0500

    mensajefirst

---
+++ ejemploclase2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ejemploclase2
+//
+//  Created by mescurra on 4/22/18.
+//  Copyright © 2018 mescurra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
