172fffbc4c22b45dbaa82d3e639a019ff0e9e42c
mescurra <harol.josue.gomez.ruiz@everis.com>
2018-07-02 16:57:49 -0500

    mensajede2

---
+++ ejemplo3/ejemplo3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ejemplo3
+//
+//  Created by mescurra on 7/2/18.
+//  Copyright © 2018 mescurra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
