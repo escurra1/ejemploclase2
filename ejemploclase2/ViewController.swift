24c1ec2d4a9c08325d2841a170a91250f13f9086
Maurihño Enrique Escurra Colquis <mescurrac123@gmail.com>
2018-07-02 15:55:46 -0500

    Update ViewController.swift

--- ejemploclase2/ViewController.swift
+++ ejemploclase2/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Xcode10")
+        print("iOS12")
     }
 
     override func didReceiveMemoryWarning() {
